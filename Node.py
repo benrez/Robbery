533c9917061284b8783ddf1bb65822b2b7bd278f
mayvid6-6 <97320735+mayvid6-6@users.noreply.github.com>
2023-02-13 15:47:53 -0500

    Create Node.py
    Did Basics

---
+++ Node.py
@@ -0,0 +1,18 @@
+class Node():
+
+  def __init__(self, prob):
+    self.probability = prob
+    self.connections = {}
+    
+  def add_simple_connection(connectingNode, pgivenNode):
+    self.connections[connectingNode] = pgivenNode
+  def add_complex_connections
+def main():
+  Burglary = new Node(0.001)
+  Earthquake = new Node(0.002)
+  Alarm = new Node()
+  JohnCalls = new Node()
+  MaryCalls = new Node()
+  
+if __name__ == '__main__':
+   main()
