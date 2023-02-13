97d632c7eb12072d6fecc94b542b8d34c5dcb0be
benrez <119080501+benrez@users.noreply.github.com>
2023-02-13 15:59:21 -0500

    Update Node.py

--- Node.py
+++ Node.py
@@ -1,12 +1,15 @@
 class Node():
 
-  def __init__(self, prob):
+  def __init__(self, prob, prob_table):
     self.probability = prob
     self.connections = {}
+    self.prob_table = prob_table
     
   def add_simple_connection(connectingNode, pgivenNode):
     self.connections[connectingNode] = pgivenNode
+    
   def add_complex_connections
+  
 def main():
   Burglary = new Node(0.001)
   Earthquake = new Node(0.002)
